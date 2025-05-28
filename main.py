f803b27a79920c5e8baeae5ebf0645bf0e372eed
cristopherdiaz7 <crl.diaz@alumno.um.edu.ar>
2025-05-28 20:23:34 -0300

    hicimos el 1er punto de buscar por titulo o artista

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+import re
 
 try:
     df = pd.read_csv("data/spotify_and_youtube.csv")
 except FileNotFoundError:
-    print("⚠️ No se encontró el archivo CSV. Asegurate de que esté en la carpeta 'data'.")
+    print("No se encontró el archivo CSV. Asegurate de que esté en la carpeta 'data'.")
     exit()
 
 
@@ -17,13 +17,37 @@ def mostrar_menu():
     print("5. Salir")
 
 
+def buscar_titulo_o_artista(df):
+    texto = input("Ingresá parte del título o artista: ").strip()
+
+    patron = re.compile(re.escape(texto), re.IGNORECASE)
+
+    resultados = df[df['Track'].apply(lambda x: bool(patron.search(str(x)))) |
+                    df['Artist'].apply(lambda x: bool(patron.search(str(x))))]
+
+    if resultados.empty:
+        print("No se encontraron coincidencias.")
+        return
+
+    col_reproducciones = "Streams" if "Streams" in resultados.columns else "Views"
+    resultados = resultados.sort_values(by=col_reproducciones, ascending=False)
+
+    print("\n Resultados encontrados:")
+    for _, fila in resultados.iterrows():
+        artista = fila["Artist"]
+        cancion = fila["Track"]
+        duracion_ms = int(fila["Duration_ms"])
+        duracion = pd.to_timedelta(duracion_ms, unit='ms')
+        tiempo = str(duracion).split(" ")[-1]
+        print(f"{artista} - {cancion} ({tiempo})")
+
 def main():
     while True:
         mostrar_menu()
         opcion = input("Seleccioná una opción: ")
 
         if opcion == "1":
-            print("Buscar por título o artista (a implementar)")
+            buscar_titulo_o_artista(df)
         elif opcion == "2":
             print("Top 10 de un artista (a implementar)")
         elif opcion == "3":
